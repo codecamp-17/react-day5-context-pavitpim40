d7a44c5c4057424d6ebd095dae871c0ca30d5333
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-06 16:30:21 +0700

    solved by Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,35 @@
+import { createContext } from 'react';
+import { useContext } from 'react';
+
+// #1 : CreateContext => ContextName
+const AppContext = createContext();
+
+// Provider : ผู้จัดหา <AppContext.Provider>
+// Consumer : ผู้ใช้ข้อมูล
+
 function App() {
-  const data = 42;
+  const sharedData = 42;
+
+  // # 2Provider : SetUp Provider Component
+  // # 3Provider : Set value ที่จะ Shared
   return (
-    <div className='flex justify-center mt-4'>
-      <Parent data={data} />
-    </div>
+    <AppContext.Provider value={sharedData}>
+      <div className='flex justify-center mt-4'>
+        <Parent />
+      </div>
+    </AppContext.Provider>
   );
 }
 
-function Parent({ data }) {
+function Parent() {
   // เป็นได้แค่ทางผ่าน prop : prop drilling
-  return <Child num={data} />;
+  return <Child />;
 }
 
-function Child({ num }) {
-  return <h1 className='text-2xl italic font-bold'>{num}</h1>;
+function Child() {
+  // #2Consume : useContext(ContextName) => ใช้ Data ได้
+  const sharedData = useContext(AppContext);
+  return <h1 className='text-2xl italic font-bold'>{sharedData}</h1>;
 }
 
 export default App;
