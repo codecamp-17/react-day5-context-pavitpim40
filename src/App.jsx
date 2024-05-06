7509dcb6f4ea11d9b12f1aabcd2e7a65974df48c
pavitpim40_mbp <pavitpim@gmail.com>
2024-05-06 16:20:20 +0700

    address problem

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ function App() {
   const data = 42;
   return (
     <div className='flex justify-center mt-4'>
-      <Parent />
+      <Parent data={data} />
     </div>
   );
 }
 
-function Parent() {
-  return <Child />;
+function Parent({ data }) {
+  // เป็นได้แค่ทางผ่าน prop : prop drilling
+  return <Child num={data} />;
 }
 
-function Child() {
-  return <h1 className='text-2xl italic font-bold'>HARD CODE VALUE</h1>;
+function Child({ num }) {
+  return <h1 className='text-2xl italic font-bold'>{num}</h1>;
 }
 
 export default App;
